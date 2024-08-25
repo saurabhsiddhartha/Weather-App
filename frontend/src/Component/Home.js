46a39d39eccccc5bc4f40d517fffa2a31a09dd4e
saurabhsiddhartha <saurabhsiddhartha82@gmail.com>
2024-08-25 14:44:14 +0530

    Project done

--- frontend/src/Component/Home.js
+++ frontend/src/Component/Home.js
@@ -1,13 +1,17 @@
-import React, { useState ,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import TempCard from "./TempCard";
+import Loading from "./Loading";
 
 const Home = () => {
-    const [city, setCity] = useState("Lucknow"); 
+    const [city, setCity] = useState("Lucknow");
     const [forecastDetail, setForecastDetail] = useState([]);
     const [weatherDetail, setWeatherDetail] = useState({});
-    const [weather ,setWeather] = useState({});
-    const [loading, setLoading] = useState(false);  
+    const [place, setPlace] = useState({})
+    const [weather, setWeather] = useState({});
+    const [iconUrl, setIconUrl] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [windSpeed , setWindSpeed] = useState('');
     const api_key = '8f110526e31c64d18e7f4539d30a357b';
 
     const handleChange = (e) => {
@@ -15,47 +19,68 @@ const Home = () => {
     }
 
     const searchCity = async () => {
-        setLoading(true);  // Start loading
+        setLoading(true);   
         try {
             const response = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`);
-            const { lat, lon } = response.data[0]; 
-            const forecastResponse = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`);
-            const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`);
-            const weatherIcon = await axios.get(`https://openweathermap.org/img/wn/10d@2x.png`)
+
+            let cityFound = false;
+
+            for (let i = 0; i < response.data.length; i++) {
+                if (city.toLowerCase() === response.data[i].name.toLowerCase()) {
+                    cityFound = true;
+                    break;  
+                }
+            }
+            if (!cityFound) {
+                alert("City not found. Please enter the correct city name.");
+            }
+ 
+            const { lat, lon } = response.data[0];
+
+            const forecastResponse = await axios.get(`https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`);
             setForecastDetail(forecastResponse.data);
-            setWeather(weatherResponse.data.weather)
-            console.log(weather[0].main)
-            // console.log(weatherResponse.data)
+            // console.log(forecastDetail)
+            // console.log(forecastDetail)
+
+            const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`);
+            const weatherData = weatherResponse.data.weather[0];
+            setWeather(weatherData);
+            setWindSpeed(weatherResponse.data.wind.speed)
             setWeatherDetail(weatherResponse.data.main);
+            setPlace(response.data[0])
+ 
+            const iconUrl = `https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
+            setIconUrl(iconUrl);
 
         } catch (error) {
             console.error('Error fetching data:', error);
         } finally {
-            setLoading(false);  // End loading
+            setLoading(false);  
         }
     };
+
     useEffect(() => {
         searchCity();
-    }, []); 
+    }, []);
 
     return (
         <>
             <div className="cover-page">
                 <h1>Current Weather</h1>
                 <div className="search-container">
-                    <input 
+                    <input
                         type="text"
                         placeholder="Enter location"
                         name="city"
-                        value={city}
+                        // value={city}
                         onChange={handleChange}
                     />
                     <button onClick={searchCity}>Search</button>
                 </div>
                 {loading ? (
-                    <p>Loading...</p>  // Loading message
+                    <Loading/> // Loading message
                 ) : (
-                    <TempCard city={city} apikey={api_key} weatherDetail={weatherDetail} weather={weather} />
+                    <TempCard city={city} apikey={api_key} weatherDetail={weatherDetail} weather={weather} iconUrl={iconUrl} forecastDetail={forecastDetail} place={place} windSpeed ={windSpeed}/>
                 )}
             </div>
         </>
